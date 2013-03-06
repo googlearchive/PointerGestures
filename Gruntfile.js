8947ebd2d2dd30daf50986c0e0bf231b124d29df
Daniel Freedman <dfreedm@google.com>
2013-03-06 10:42:07 -0800

    Flatten grunt build
    - Recursive grunt with hub requires sub packages to have grunt and all contrib
      packages installed as well
    
    - Does not scale well to many levels of nesting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,27 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-hub');
+
   grunt.initConfig({
-    hub: {
-      pe: {
-        src: 'src/PointerEvents/Gruntfile.js'
-      }
-    },
     uglify: {
       pointergestures: {
+        options: {
+          sourceMap: 'build/pointergestures.js.map',
+          sourceMappingURL: 'pointergestures.js.map',
+          sourceMapRoot: '..'
+        },
         dest: 'build/pointergestures.js',
         src: [
-          'src/PointerEvents/build/pointerevents.js',
+          // PointerEvents
+          'src/PointerEvents/src/touch-action.js',
+          'src/PointerEvents/src/PointerEvent.js',
+          'src/PointerEvents/src/pointermap.js',
+          'src/PointerEvents/src/sidetable.js',
+          'src/PointerEvents/src/dispatcher.js',
+          'src/PointerEvents/src/installer.js',
+          'src/PointerEvents/src/findTarget.js',
+          'src/PointerEvents/src/platform-events.js',
+          'src/PointerEvents/src/capture.js',
+          // PointerGestures
           'src/PointerGestureEvent.js',
           'src/initialize.js',
           'src/sidetable.js',
@@ -25,5 +35,6 @@ module.exports = function(grunt) {
       }
     }
   });
-  grunt.registerTask('default', ['hub', 'uglify']);
+
+  grunt.registerTask('default', 'uglify');
 };
