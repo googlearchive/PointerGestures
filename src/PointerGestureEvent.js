0c9d197048be75b95201a04ac04fe052b0a19b48
Daniel Freedman <dfreedm@google.com>
2013-03-28 16:01:40 -0700

    simplify Gesture Event creation

--- src/PointerGestureEvent.js
+++ src/PointerGestureEvent.js
@@ -15,44 +15,29 @@
  */
 
 function PointerGestureEvent(inType, inDict) {
-  var e = document.createEvent('UIEvent');
-  if (Object.__proto__) {
-    e.__proto__ = PointerGestureEvent.prototype;
-    e.initGestureEvent(inType, inDict);
-  } else {
-    PointerGestureEvent.prototype.initGestureEvent.call(e, inType, inDict);
-  }
-  return e;
-}
-
-PointerGestureEvent.prototype = Object.create(UIEvent.prototype);
-
-/**
- * Initialize the PointerGestureEvent with an event type, and a dictionary of
- * properties to set.
- *
- * @method initGestureEvent
- * @param {String} inType Event type
- * @param {Object} [inDict] Dictionary of properties to initialize on the event
- */
-PointerGestureEvent.prototype.initGestureEvent = function(inType, inDict) {
+  var dict = inDict || {};
+  var e = document.createEvent('Event');
   var props = {
     bubbles: true,
     cancelable: true,
-    view: null,
-    detail: null
   };
+  Object.keys(props).forEach(function(k) {
+    if (k in dict) {
+      props[k] = dict[k];
+    }
+  });
 
-  for (var k in inDict) {
-    props[k] = inDict[k];
-  }
+  e.initEvent(inType, props.bubbles, props.cancelable);
 
-  this.initUIEvent(inType, props.bubbles, props.cancelable, props.view, props.detail);
+  Object.keys(dict).forEach(function(k) {
+    e[k] = inDict[k];
+  });
 
-  for (var k in props) {
-    this[k] = props[k];
-  }
-};
+  e.preventTap = this.preventTap;
+
+  return e;
+}
+
 /**
  * Allows for any gesture to prevent the tap gesture.
  *
