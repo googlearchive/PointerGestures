47ddae73ebd83a72b4a56a590f6a1800fe94f5e2
Daniel Freedman <dfreedm@google.com>
2013-04-23 11:49:24 -0700

    update PointerEvents, modernize sidetable

--- src/sidetable.js
+++ src/sidetable.js
@@ -7,22 +7,31 @@
 // SideTable is a weak map where possible. If WeakMap is not available the
 // association is stored as an expando property.
 (function(scope) {
+(function(scope) {
   var SideTable;
   // TODO(dfreedman): WeakMap does not allow for Events to be keys in Firefox
   if (typeof WeakMap !== 'undefined' && navigator.userAgent.indexOf('Firefox/') < 0) {
     SideTable = WeakMap;
   } else {
-    SideTable = function(name) {
-      this.name = '__$' + name + '$__';
+    var defineProperty = Object.defineProperty;
+    var hasOwnProperty = Object.hasOwnProperty;
+    var counter = new Date().getTime() % 1e9;
+
+    SideTable = function() {
+      this.name = '__st' + (Math.random() * 1e9 >>> 0) + (counter++ + '__');
     };
+
     SideTable.prototype = {
       set: function(key, value) {
-        Object.defineProperty(key, this.name, {value: value, writable: true});
+        defineProperty(key, this.name, {value: value, writable: true});
       },
       get: function(key) {
-        return key[this.name];
+        return hasOwnProperty.call(key, this.name) ? key[this.name] : undefined;
+      },
+      delete: function(key) {
+        this.set(key, undefined);
       }
-    }
+    };
   }
   scope.SideTable = SideTable;
 })(window.PointerGestures);
