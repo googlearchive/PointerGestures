e73f6dc989ac2fb446754000a5ab5a04baf6b48b
Yvonne Yip <ykyyip@google.com>
2013-06-20 16:06:53 -0700

    test: fix file paths

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(karma) {
   karma.configure({
     // base path, that will be used to resolve files and exclude
-    basePath: '',
+    basePath: '../',
 
 
     // frameworks to use
@@ -10,27 +10,27 @@ module.exports = function(karma) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/chai/chai.js',
-      'node_modules/chai-spies/chai-spies.js',
-      '../PointerEvents/src/boot.js',
-      '../PointerEvents/src/PointerEvent.js',
-      '../PointerEvents/src/pointermap.js',
-      '../PointerEvents/src/sidetable.js',
-      '../PointerEvents/src/dispatcher.js',
-      '../PointerEvents/src/installer.js',
-      '../PointerEvents/src/platform-events.js',
-      '../PointerEvents/src/capture.js',
-      'src/PointerGestureEvent.js',
-      'src/initialize.js',
-      'src/sidetable.js',
-      'src/pointermap.js',
-      'src/dispatcher.js',
-      'src/hold.js',
-      'src/track.js',
-      'src/flick.js',
-      'src/tap.js',
-      'tests/setup.js',
-      'tests/!(setup).js'
+      'PointerGestures/node_modules/chai/chai.js',
+      'PointerGestures/node_modules/chai-spies/chai-spies.js',
+      'PointerEvents/src/boot.js',
+      'PointerEvents/src/PointerEvent.js',
+      'PointerEvents/src/pointermap.js',
+      'PointerEvents/src/sidetable.js',
+      'PointerEvents/src/dispatcher.js',
+      'PointerEvents/src/installer.js',
+      'PointerEvents/src/platform-events.js',
+      'PointerEvents/src/capture.js',
+      'PointerGestures/src/PointerGestureEvent.js',
+      'PointerGestures/src/initialize.js',
+      'PointerGestures/src/sidetable.js',
+      'PointerGestures/src/pointermap.js',
+      'PointerGestures/src/dispatcher.js',
+      'PointerGestures/src/hold.js',
+      'PointerGestures/src/track.js',
+      'PointerGestures/src/flick.js',
+      'PointerGestures/src/tap.js',
+      'PointerGestures/tests/setup.js',
+      'PointerGestures/tests/!(setup).js'
     ],
 
 
